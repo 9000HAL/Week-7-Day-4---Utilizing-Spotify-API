55368e14326e4c1100240ec9d6c07ab974735b5e
9000HAL <gsanchezscs@gmail.com>
2023-06-08 15:34:33 -0700

    opening commit ----- SAVIN?!?!

---
+++ main_blank_thieves (2).js
@@ -0,0 +1,24 @@
+// spotify API creds
+
+
+// getToken function
+
+
+// verify that we get our token
+// getToken()
+
+// use getToken function, to get a song
+// getSong Function
+
+
+// verify that we get the song data
+
+
+// clickedSong function
+
+
+
+// handles playing the audio
+
+
+// handles pausing the audio
